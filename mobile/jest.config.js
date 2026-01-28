3215a4803baaac933ea2e8935496a7711404af99
notxplosion <greglajoie55@gmail.com>
2026-01-27 20:39:24 -0500

    tests for MapView Component

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -1,3 +1,13 @@
 module.exports = {
     preset: "jest-expo",
+    transformIgnorePatterns: [
+        "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg)"
+    ],
+    collectCoverageFrom: [
+        "**/*.{ts,tsx}",
+        "!**/coverage/**",
+        "!**/node_modules/**",
+        "!**/babel.config.js",
+        "!**/jest.setup.js"
+    ]
 };
