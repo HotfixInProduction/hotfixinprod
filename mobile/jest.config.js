279f259d19248ceacfdc9976332e44ae3c3948ce
nicolas-zampieri <156842924+nicolas-zampieri@users.noreply.github.com>
2026-02-04 19:12:20 -0500

    fix(mobile): exclude e2e from coverage collection

--- mobile/jest.config.js
+++ mobile/jest.config.js
@@ -10,6 +10,7 @@ module.exports = {
         "!**/node_modules/**",
         "!**/babel.config.js",
         "!**/jest.setup.js",
+        "!**/e2e/**",
         "!index.ts"
     ],
     reporters: [
